5f9ef16cf03d12ff9b0e50babdf02cd746f16aff
Tomasz Semkiw <tsemkiw@collab.net>
2020-10-21 10:06:43 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p> Hi there!How are you </p>
+        <p> Hi there!How are you learn react </p>
         <a
           className="App-link"
           href="https://reactjs.org"
